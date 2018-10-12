816306c2b23f02045d3443ae5cbb86aa81af080c
BT_1N3_15 <jfreund5400@bths.edu>
2018-10-12 14:47:49 -0400

    objective 2

--- src/Game.java
+++ src/Game.java
@@ -3,24 +3,23 @@ import java.util.Scanner;
 
 public class Game {
 
-    public static void main(String[] args){
-        Scanner first=new Scanner(System.in);
-        int n=0;
+    public static void main(String[] args) {
+        Scanner first = new Scanner(System.in);
+        int n = 0;
         System.out.println("What difficulty would you like to do? Type 1, 2, 3, or 4?");
-        n=first.nextInt();
-        int num=1+(int)(Math.random()*(Math.pow(10,n)));
-        Scanner input=new Scanner(System.in);
-        int guess=0;
+        n = first.nextInt();
+        int num = 1 + (int) (Math.random() * (Math.pow(10, n)));
+        Scanner input = new Scanner(System.in);
+        int guess = 0;
         System.out.println("Guess my secret number:");
-        guess=input.nextInt();
-        int numguesses=1;
-        System.out.println(num);
-        while(guess!=num){
+        guess = input.nextInt();
+        int numguesses = 1;
+        while (guess != num) {
             System.out.println("Guess again:");
-            guess=input.nextInt();
-            numguesses+=1;
+            guess = input.nextInt();
+            numguesses += 1;
         }
-        System.out.println("You got it right! My secret number is "+num+".");
-        System.out.println("You guessed "+numguesses+" times");
+        System.out.println("You got it right! My secret number is " + num + ".");
+        System.out.println("You guessed " + numguesses + " times");
     }
 }
