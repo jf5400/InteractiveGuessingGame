6d854248a9b36ba7d7c11cde71a58866ba665f4a
jazminef <41169790+jazminef@users.noreply.github.com>
2018-10-14 20:49:45 -0400

    objective 2

--- src/Objective2.java
+++ src/Objective2.java
@@ -4,25 +4,29 @@ import java.util.Scanner;
 public class Objective2 {
     public static void main(String[] args) {
         System.out.println("You are going to guess a number from 1-100,");
-        System.out.println("Then I am going to guess your number and you are going to say if it is higher, lower, or correct.");
-        int lower=1;
-        int higher=100;
+        System.out.println("Then I am going to guess your number and you are going to tell if it is higher, lower, or correct.");
+        System.out.println("If it is lower do 1, correct do 2, and higher do 3 ");
+        int lower=0;
+        int higher=101;
         int guess=(int)(Math.random()*100)+1;
         Scanner input= new Scanner(System.in);
-        String confirmation = "";
+        int confirmation = 1;
         System.out.println("Is your number "+guess+"?");
-        confirmation=input.nextLine();
-        while (confirmation != "correct") {
-            if(confirmation=="higher"){
+        confirmation=input.nextInt();
+        if(confirmation==1){
+            System.out.println("yes");
+        }
+        while (confirmation != 2) {
+            if(confirmation==3){
                 lower=guess;
             }
-            else if(confirmation=="lower"){
+            else if(confirmation==1){
                 higher=guess;
             }
-            guess=(int)(Math.random()*higher)+lower;
-            System.out.println(lower+","+higher);
+            guess=((int)(Math.random()*(higher-1)))+(1+lower);
             System.out.println("Is your number "+guess+"?");
-            confirmation=input.nextLine();
+            confirmation=input.nextInt();
+
         }
         System.out.println("I guessed your number correctly!");
     }
